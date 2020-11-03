3c6d6d83c81450bf78e0791417a655a4863acd96
sunminJeong <tjsals7825@gmail.com>
2020-11-03 22:57:44 +0900

    ~#1.1 apollo test

--- src/apollo.js
+++ src/apollo.js
@@ -2,6 +2,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 // https://www.apollographql.com/docs/react/get-started/
 const client = new ApolloClient({
+    // cors 설정 필요함..
     uri: 'https://yts.mx/api/v2/list_movies.json',
     cache: new InMemoryCache()
 });
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,2 +1,31 @@
 import React from 'react';
-export default () => <h1>Home</h1>;
+import { gql, useQuery } from '@apollo/client';
+
+const GET_MOVIES = gql`
+    {
+        movies {
+            id
+            medium_cover_image
+        }
+    }
+`;
+export default () => {
+    // useQuery is a React hook that use the Hooks API to share GraphQL data with your UI
+    const { loading, error, data } = useQuery(GET_MOVIES);
+    console.log(error, data)
+    if (loading) {
+        return <p>Loading...</p>;
+    }
+    if (error) {
+        return <p>Error : {error.message}</p>;
+    }
+    if (data && data.movies) {
+        return data.movies.map(({ id, medium_cover_image }) => (
+            <div key={id}>
+                <p>
+                    {id}
+                </p>
+            </div>
+        ));
+    }
+};
